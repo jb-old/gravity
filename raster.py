90fef3bc292952e6a59b4a103e0c02e00dde904f
Jeremy Banks <jeremy@jeremybanks.ca>
2009-12-31 14:32:04 -0500

    that data doesn't look right

--- raster.py
+++ raster.py
@@ -244,8 +244,10 @@ def main():
 
     print("Image instantiated.")
     
-    for p in range(size):
+    for p in range(size + 1):
         r, g, b, a = mah_spectrum(p / size)
+        print("{:3d}, {:3d} ({:2f}) | {:2f} {:2f} {:2f} | {:2f}"
+              .format(p, p, p / size, r, g, b, a))
         image.speck([p, p], [r, g, b], a)
     
     print("Image generated.")
