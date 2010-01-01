ce3a10c9fa869566e0ba730901c242a1feb3241d
Jeremy Banks <jeremy@jeremybanks.ca>
2010-01-01 13:46:40 -0500

    kinda sort better

--- raster.py
+++ raster.py
@@ -73,7 +73,7 @@ class Raster(Object):
         
         self.data[i:i + self.color_fmt.size] = self.color_fmt.pack(*color)
 
-    def dot(self, coordinates, color, not_darkness=1, pen=None, radius=2.5):
+    def dot(self, coordinates, color, not_darkness=1, pen=None, radius=2):
         """Draws a dot/circle of the chosen radius. Default, .5, is just a point."""
 
         pen = pen or self.pen
@@ -87,14 +87,12 @@ class Raster(Object):
                 distance = math.sqrt(x_o ** 2 +
                                      y_o ** 2)
                 
-                far_out = distance - radius
-                
-                if far_out <= -.5:
+                if distance <= radius - .5:
                     self.point((x + x_o,
                                 y + y_o), color, not_darkness, pen)
-                elif far_out < .5:
+                elif distance <= radius + .5:
                     self.point((x + x_o,
-                                y + y_o), color, not_darkness * (far_out + .5), pen)
+                                y + y_o), color, not_darkness * (distance - radius + .5), pen)
     
                               # type # value # description
                               # ---- # ----- # -----------
