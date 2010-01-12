4b53f951987b92e8cee4f7e9bee60aceb9b56f49
Jeremy Banks <jeremy@jeremybanks.ca>
2010-01-11 21:36:32 -0500

    nasty

--- gravity.py
+++ gravity.py
@@ -4,12 +4,16 @@
 import sys
 import raster
 from math import sqrt # since you can just do **.5 why isn't this builtin?
+from math import sin, cos
 
 class Object(object):
     def __init__(self, mass, velocity, position):
         self.mass = mass
         self.velocity = list(velocity)
         self.position = list(position)
+
+    def distance_from(self, other):
+        return sum(map((lambda a_b: (a_b[0] - a_b[1]) ** 2), zip(self.position, other.position)))**.5
 
 class GravitySim(object):
     def __init__(self, initial_state, resolution=10, G=6.67428e-11):
@@ -23,8 +27,24 @@ class GravitySim(object):
         next_frame = list()
         
         for object in self.state:
-            # apply gravity
             new_velocity = object.velocity
+
+            for other in self.state:
+                if other is not object:
+                    a_magnitude = other.mass / object.distance_from(other) ** 2
+                    
+                    # this is almost certainly wrong:
+                    x_delta = object.position[0] - other.position[0]
+                    y_delta = object.position[1] - other.position[1]
+                    x_portion = x_delta / abs(x_delta + y_delta)
+                    y_portion = y_delta / abs(x_delta + y_delta)
+                    a_x = a_magnitude * x_portion
+                    a_y = a_magnitude * y_portion
+
+                    a = [a_x, a_y]
+
+                    new_velocity = map(sum, zip(a, new_velocity))
+            
             next_frame.append(Object(object.mass, new_velocity, map(sum, zip(object.position, new_velocity))))
         
         self.frames.append(next_frame)
@@ -58,7 +78,7 @@ def main(frames=100):
         r, g, b, a = raster.mah_spectrum(f / (frames - 1))
         
         for object in sim.state:
-            image.dot(map(sum, zip(object.position, offset)), [r, g, b], a, radius=sqrt(object.mass) * 3)
+            image.dot(list(map(sum, zip(object.position, offset))), [r, g, b], a, radius=sqrt(object.mass) * 3)
     
     with open("out.bmp", "wb") as o:
         image.write_bmp(o)
