7a9e9d3d9eaa6708ba3b26011fc9e488806df726
Jeremy Banks <jeremy@jeremybanks.ca>
2010-01-12 04:25:25 -0500

    great progress, to be reading from json, added example

--- gravity.py
+++ gravity.py
@@ -56,12 +56,22 @@ class Object(object):
         self.mass = mass
         self.displacement = Vector(displacement)
         self.velocity = Vector(velocity)
+    
+    @classmethod
+    def from_dict(cls, source_dict):
+        input_dict = from_dict.default_dict
+        input_dict.update(source_dict)
+        
+        return cls(input_dict["m"], input_dict["d"], input_dict["v"])
+    from_dict.default_dict = { "m": 1, "d": [ 0, 0 ], "v": [ 0, 0 ], "comment": None }
 
 class System(list):
-    def advanced(self, dt_per_step, steps=1, G=6.67428e-11):
+    def advanced(self, dt, frames=1, G=6.67428e-11):
         current = System(self)
 
-        for _ in range(steps):
+        dt_per_frame = dt / frames
+        
+        for _ in range(fames):
             next = System()
             
             for old_object in self:
@@ -71,90 +81,74 @@ class System(list):
                     if other is not old_object:
                         displacement = old_object.displacement - other.displacement
                         
-                        if displacement: # they aren't at the same position
+                        if displacement: # if they aren't at the same position
                             acceleration_magnitude = G * other.mass / displacement.magnitude ** 2
-                            
-                            
-                            
-                        
+
+                            x_to_y = displacement[0] / (abs(v) for v in displacement)
+
+                            acceleration = V(acceleration_magnitude * x_to_y,
+                                             acceleration_magnitude * (1 - x_to_y))
+
+                            new_object.velocity += acceleration * dt_per_frame
+            
             current = next
         
         return current # Python not having tail recursion optimization is stupid.
                        # If it did, I'd implement this recursively and be happier.
-    
-    def tick(self):
-        d_t = 1 / self.resolution
-        
-        next_frame = list()
-        
-        for object in self.state:
-            new_velocity = object.velocity
 
-            for other in self.state:
-                if other is not object:
-                    a_magnitude = self.G * other.mass / (object.distance_from(other) ** 2)
-                    
-                    # this is almost certainly wrong:
-                    x_delta = object.position[0] - other.position[0]
-                    y_delta = object.position[1] - other.position[1]
-                    x_portion = -x_delta / ( abs(x_delta) + abs(y_delta)) if x_delta or y_delta else 0
-                    y_portion = -y_delta / ( abs(x_delta) + abs(y_delta)) if x_delta or y_delta else 0
-                    a_x = a_magnitude * x_portion
-                    a_y = a_magnitude * y_portion
-
-                    a = [a_x, a_y]
-
-                    new_velocity = list(map(sum, zip(a, new_velocity)))
-            
-            next_frame.append(Object(object.mass, new_velocity, map(sum, zip(object.position, new_velocity))))
+def starify_raster(raster, n=200):
+    """Draws background-ish "stars" on a Raster image."""
+    for _ in range(100):
+        x = random.random() * (raster.width + 4) - 2
+        y = random.random() * (raster.height + 4) - 2
         
-        self.frames.append(next_frame)
-    
-    def tock(self, seconds):
-        for _ in range(int(resolution * seconds)):
-            self.tick()
-    
-    @property
-    def state(self):
-        return self.frames[-1]
+        r, g, b = [ .5 + .5 * random.ranom() for _ in range(3) ]
+        
+        g = min(r, min(b, g)) # ensure green is never above red or blue
+        # maybe I could draw colours from a gradient instead, and use one limiting green by default?
+        
+        raster.dot([x, y], [r, g, b], 1.0, random.random() * 1.2)
 
 import sys
 import raster
 
+raster.Raster.starify = starify_raster
+
 def main(in_filename="-", out_filename="-"):
-    in_ = open(in_filename,  "rt") if in_filename  != "-" else sys.stdin
-    out = open(out_filename, "wb") if out_filename != "-" else sys.stdout.buffer
-    # G = 
-    import json
+    in_file  = open(in_filename,  "rt") if in_filename  != "-" else sys.stdin
+    out_file = open(out_filename, "wb") if out_filename != "-" else sys.stdout.buffer
     
-    with in_, out:
-        pass
+    # later I should add many more rendering control options to this, but this will do to start
+    input_defaults = { "comment": None, # it's a comment, ignored
+                       "dimensions": [ 256, 256 ], # size of output image, should have some zoom/positioning
+                       "G": 6.67428e-11, # gravitational constant (you will want to set this, ha ha maybe I should fix that)
+                       "dt": 1.0, # seconds elasped in render
+                       "frames": 100, # physics and drawing "frames" to use
+                       "objects": [] } # objects in system we're rendering
     
-    frames = int(frames)
-    
-    sim = GravitySim([ Object(10, [ 0, +0.3], [ 0,  0]),
-                       Object( 2, [ 0, -1.5], [30,  0]), ],
-                       Object(5, [ -.1,   1], [-64, -64]) ],
-                     G=10, resolution=15) # only diffs reality by a factor of 150 billion
-    
-    width = height = 192
-    offset = [ width / 2, height / 2]
-                              
-    image = raster.Raster_24RGB(width, height, fill=[0, 0, 0], pen=raster.PEN_MAX)
-    
-    for f in range(frames):
-        print("Generating frame {}...".format(f))
+    with in_file, out_file:
+        input_dict = input_defaults
+        input_dict.update(json.load(in_file))
+
+        width, height = dimensions = input_dict["dimensions"]
+        offset = [ width / 2, height / 2 ]
+
+        frame_dt = input_dict["dt"] / (input_dict["frames"] - 1) or 1
         
-        if f: sim.tick()
+        image = raster.Raster_24RGB(width, height, fill=[0, 0, 0], pen=raster.PEN_MAX)
+        image.starify()
+
+        system = System(Object.from_dict(d) for d in input_dict["objects"])
         
-        r, g, b, a = raster.mah_spectrum(f / (frames - 1))
-        
-        for object in sim.state:
-            image.dot([ object.position[0] + offset[0],
-                        object.position[1] + offset[1] ], [r, g, b], a, radius=sqrt(object.mass) * 3)
-    
-    with open("out.bmp", "wb") as o:
-        image.write_bmp(o)
+        for f in range(input_dict["frames"]):
+            if f: system = system.advance(frame_dt)
+
+            r, g, b, a = raster.maybe(f / (frames - 1))
+
+            for object in system:
+                image.dot(object.position + offset, [r, g, b], a, raster=object.mass ** .5 * 2)
+
+        image.write_bmp(out_file)
     
 if __name__ == "__main__":
     sys.exit(main(*sys.argv[1:]))
