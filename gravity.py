0484470168a93f36943477a674e3925d1ed0d857
Jeremy Banks <jeremy@jeremybanks.ca>
2010-01-16 14:54:58 -0500

    progress, not working atm, though

--- gravity.py
+++ gravity.py
@@ -1,63 +1,12 @@
 #!/usr/bin/env python3.1
-import itertools
-
-class Vector(object):
-    """A Vector value of any number of dimensions."""
-    
-    def __init__(self, components):
-        self.components = list(components)
-
-    @property
-    def magnitude(self):
-        return sum(x ** 2 for x in self.components) ** (1/2)
-    
-    @magnitude.setter
-    def magnitude(self, value):
-        self *= value / self.magnitude
-    
-    def __getitem__(self, index):
-        return self.components[index]
-    
-    def __setitem__(self, index, value):
-        self.components[index] = value
-    
-    def __add__(self, vector):
-        return type(self)(a + b for (a, b) in itertools.zip_longest(self.components, vector.components, fillvalue=0))
-    
-    def __sub__(self, vector):
-        return type(self)(a - b for (a, b) in itertools.zip_longest(self.components, vector.components, fillvalue=0))
-    
-    def __mul__(self, scalar):
-        return type(self)(x * scalar for x in self.components)
-
-    __rmul__ = __mul__
-    
-    def __truediv__(self, scalar):
-        return type(self)(x / scalar for x in self.components)
-
-    def __neg__(self):
-        return type(self)(-x for x in self.components)
-
-    def __iter__(self):
-        return iter(self.components)
-
-    def __bool__(self):
-        return any(self)
-
-    def __eq__(self, other):
-        return self.components == other.components
-
-    def __repr__(self):
-        return "V({})".format(", ".join(repr(v) for v in self.components))
-
-    # use shifts to repr rotations cw and ccw
-
-def V(*components):
-    """Shortcut for creating Vectors: V(x, y...) -> Vector((x, y...))"""
-    
-    return Vector(components)
-
 from copy import copy, deepcopy
+import itertools
+import json
+import random
+import sys
+
+from vector import Vector, V, sqrt
+import raster
 
 class Object(object):
     """A non-elastic frictionless sphere in a vaccum, ha."""
@@ -68,32 +17,34 @@ class Object(object):
         self.velocity = Vector(velocity)
         
         if radius is None:
-            self.radius = 2 * self.mass ** (1/2)
+            self.radius = 2 * sqrt(self.mass)
         else:
             self.radius = radius
 
         self.combining = bool(combining)
+
+    from_dict_defaults = { "m": 1, "d": [ 0, 0 ], "v": [ 0, 0 ],
+                           "radius": None, "combining": True }
     
     @classmethod
-    def from_dict(cls, source_dict,
-                  default_dict = { "m": 1, "d": [ 0, 0 ], "v": [ 0, 0 ],
-                                   "radius": None, "combining": True }):
-        input_dict = deepcopy(default_dict)
+    def from_dict(cls, source_dict):
+        input_dict = deepcopy(cls.from_dict_defaults)
         input_dict.update(source_dict)
         return cls(input_dict["m"], input_dict["d"], input_dict["v"], input_dict["radius"], input_dict["combining"])
 
     def __repr__(self):
-        return ("{.__name__}(mass={!r}, displacement={!r}, velocity={!r})"
-                .format(type(self), self.mass, self.displacement, self.velocity))
+        return ("{.__name__}(mass={!r}, displacement={!r}, velocity={!r}, radius={!r}}, combining={!r})"
+                .format(type(self), self.mass, self.displacement, self.velocity, self.radius, self.combining))
 
 def simulate(current, time_step, G=6.67428e-11):
-    """From an initial situation, infinitely yield the following frames."""
-    dt_per_frame = dt / frames
+    """Yields an initial state and all following frames."""
+    
+    yield current
     
     while True:
-        next = deepcopy(current)
+        current = deepcopy(current)
         
-        for object, other in itertools.combinations(current):
+        for object, other in itertools.combinations(current, 2):
             displacement = other.displacement - object.displacement
             
             if displacement.magnitude > .5:
@@ -104,17 +55,16 @@ def simulate(current, time_step, G=6.67428e-11):
                 
                 acceleration = V(x_portion * acceleration_magnitude,
                                  y_portion * acceleration_magnitude)
-                
-                object.velocity = new_object.velocity + acceleration * dt_per_frame
+                object.velocity += acceleration * time_step
         
         for object in current:
-            object.displacement += new_object.velocity * dt_per_frame
+            object.displacement += object.velocity * time_step
         
         # combing colliding combining objects
-        for i, object in enumerate(next):
+        for i, object in enumerate(current):
             if object is None or not object.combining: continue
             
-            for j, other in enumerate(next[i + 1:]):
+            for j, other in enumerate(current[i + 1:]):
                 if other is None or not other.combining: continue
                 
                 displacement = other.displacement - object.displacement
@@ -123,23 +73,24 @@ def simulate(current, time_step, G=6.67428e-11):
                     object_portion = object.mass / (object.mass + other.mass)
                     other_portion  = other.mass  / (other.mass + other.mass)
                     
-                    combined_object = Object(object.mass + other.mass,
-                                             object_portion * object.displacement + other_portion * other.displacement,
-                                             object_portion * object.velocity     + other_portion * other.velocity,
-                                             (object.radius ** 2 + other.radius ** 2) ** (1/2))
+                    combined_object = Object(mass=object.mass + other.mass,
+                                             displacement=object_portion * object.displacement + other_portion * other.displacement,
+                                             velocity=object_portion * object.velocity + other_portion * other.velocity,
+                                             radius=sqrt(object.radius ** 2 + other.radius ** 2))
                     
-                    next[i] = object = combined_object
-                    next[i + 1 + j] = other = None
-            
-            if None in next:
-                next = [ o for o in next if o is not None ]
+                    current[i] = object = combined_object
+                    current[i + 1 + j] = other = None
         
-        current = next
+        if None in current:
+            current = [ o for o in current if o is not None ]
+        
+        yield current
 
-import random
-
-def starify_raster(raster, n=200):
+def starify_raster(raster, n=None):
     """Draws background-ish "stars" on a Raster image."""
+    
+    if n is None:
+        n = int((raster.width * raster.height) * 0.01)
     
     for _ in range(n):
         x = random.random() * (raster.width + 4) - 2
@@ -148,13 +99,9 @@ def starify_raster(raster, n=200):
         r, g, b = [ .6 + .2 * random.random() + .2 * random.random() for _ in range(3) ]
         
         g = min(r, g, b) # ensure green is never above red or blue
-        # maybe I could draw colours from a gradient instead, and use one limiting green by default?
+        # maybe I could draw colours from a gradient instead
 
         raster.dot([x, y], [r, g, b], random.random() * .7, radius=random.random())
-
-import sys
-import raster
-import json
 
 raster.Raster.starify = starify_raster
 
@@ -162,17 +109,14 @@ def main(in_filename="-", out_filename="-"):
     in_file  = open(in_filename,  "rt") if in_filename  != "-" else sys.stdin
     out_file = open(out_filename, "wb") if out_filename != "-" else sys.stdout.buffer
     
-    # later I should add many more rendering control options to this, but this will do to start
     input_defaults = { "comment": None, # it's a comment, ignored
                        "dimensions": [ 1024, 512 ], # size of output image, and unzoomed view area in metres
                        "G": 6.67428e-11, # gravitational constant
                        "dt": 60 * 60 * 24 * 365, # duration in render in seconds
                        "frames": 501, # drawing "frames" to use
-                       "ticks_per_frame": 20, # how many physics frames to use for each image frame
                        "objects": [], # objects in system we're rendering
                        "centre": [0, 0], # centre of view
                        "zoom": 1e-10 } # factor of magnification
-    # todo: make values realistic
     
     with in_file, out_file:
         input_dict = deepcopy(input_defaults)
@@ -181,39 +125,36 @@ def main(in_filename="-", out_filename="-"):
         width, height = dimensions = input_dict["dimensions"]
         offset = V(width / 2 + .5, height / 2 + .5)
         
-        frames = input_dict["frames"]
-        frame_dt = input_dict["dt"] / ((input_dict["frames"] - 1) or 1)
+        frame_count = input_dict["frames"]
+        time_step = input_dict["dt"] / ((frame_count - 1) or 1)
         centre = Vector(input_dict["centre"])
         zoom = input_dict["zoom"]
         
         sys.stderr.write("Loading input system...\n")
-        system = System(Object.from_dict(d) for d in input_dict["objects"])
+        system = [ Object.from_dict(d) for d in input_dict["objects"] ]
         
         sys.stderr.write("Instantiating image...\n")
         image = raster.Raster_24RGB(width, height, fill=[0, 0, 0], pen=raster.PEN_MAX)
-        sys.stderr.write("Rendering background \"stars\"...\n")
-        image.starify(int((width * height) / 100))
+        sys.stderr.write("Rendering background stars...\n")
+        image.starify()
         
-        def callback(frame, old, new):
-            if frame % input_dict["ticks_per_frame"] != 0:
-                return
-            
-            r, g, b, a = raster.mah_spectrum(frame / (frames - 1) if frames > 1 else .5)
-            sys.stderr.write("Calculuated, rendering frame {}...\r".format(frame))
+        frames = itertools.islice(simulate(system, time_step, G=input_dict["G"]), frame_count)
+        
+        for f, objects in enumerate(frames):
+            r, g, b, a = raster.mah_spectrum(f / (frame_count - 1) if frame_count > 1 else .5)
+            sys.stderr.write("Calculuated, rendering frame {}...\r".format(f))
             sys.stderr.flush()
             
             if a:
-                for object in new:
+                for object in objects:
                     dot_position = (object.displacement - centre) * zoom + offset
                     dot_radius = object.radius * zoom
                     
                     image.dot(dot_position, [r, g, b], a, radius=dot_radius)
         
-        callback(0, None, system) # to draw input frame
-        system = system.advanced(input_dict["dt"], input_dict["frames"] * input_dict["ticks_per_frame"], input_dict["G"], callback)
-
         sys.stderr.write("\n")
         sys.stderr.write("Writing image to file...")
+        sys.stderr.flush()
         image.write_bmp(out_file)
     
 if __name__ == "__main__":
---
+++ vector.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3.1
+import itertools
+
+def sqrt(n):
+    """Returns the square root of a number.
+
+    The default math.sqrt only works on floats, you see."""
+    
+    return n ** .5
+
+class Vector(object):
+    """A Vector value of any number of dimensions."""
+    
+    def __init__(self, components):
+        self.components = list(components)
+
+    @property
+    def magnitude(self):
+        return sum(x ** 2 for x in self.components) ** (1/2)
+    
+    @magnitude.setter
+    def magnitude(self, value):
+        self *= value / self.magnitude
+    
+    def __getitem__(self, index):
+        return self.components[index]
+    
+    def __setitem__(self, index, value):
+        self.components[index] = value
+    
+    def __add__(self, vector):
+        return type(self)(a + b for (a, b) in itertools.zip_longest(self.components, vector.components, fillvalue=0))
+    
+    def __iadd__(self, vector):
+        self.components = [a + b for (a, b) in itertools.zip_longest(self.components, vector.components, fillvalue=0)]
+        return self
+    
+    def __sub__(self, vector):
+        return type(self)(a - b for (a, b) in itertools.zip_longest(self.components, vector.components, fillvalue=0))
+    
+    def __isub__(self, vector):
+        self.components = [a - b for (a, b) in itertools.zip_longest(self.components, vector.components, fillvalue=0)]
+        return self
+    
+    def __mul__(self, scalar):
+        return type(self)(x * scalar for x in self.components)
+    
+    __rmul__ = __mul__
+    
+    def __imul__(self, scalar):
+        self.components = [x * scalar for x in self.components]
+        return self
+    
+    def __truediv__(self, scalar):
+        return type(self)(x / scalar for x in self.components)
+    
+    def __itruediv__(self, scalar):
+        self.components = [x / scalar for x in self.components]
+        return self
+    
+    def __floordiv__(self, scalar):
+        return type(self)(x // scalar for x in self.components)
+    
+    def __ifloordiv__(self, scalar):
+        self.components = [x // scalar for x in self.components]
+    
+    def __neg__(self):
+        return type(self)(-x for x in self.components)
+
+    def __iter__(self):
+        return iter(self.components)
+
+    def __bool__(self):
+        return any(self)
+
+    def __eq__(self, other):
+        return self.components == other.components
+
+    def __repr__(self):
+        return "V({})".format(", ".join(repr(v) for v in self.components))
+
+    # use shifts to repr rotations cw and ccw
+
+def V(*components):
+    """Shortcut for creating Vectors: V(x, y...) -> Vector((x, y...))"""
+    
+    return Vector(components)
